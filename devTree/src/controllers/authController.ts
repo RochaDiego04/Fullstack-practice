c48dfb3f3f470b16b90e6dae464519e667d5d9a3
Diego Rocha <drochaesc@gmail.com>
2026-02-15 19:25:57 -0600

    auth: added express validator for server side validation

--- devTree/src/controllers/authController.ts
+++ devTree/src/controllers/authController.ts
@@ -1,19 +1,36 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
+import slug from "slug";
 import { hashPassword } from "../utils/auth";
+import { validationResult } from "express-validator";
 
 export const createAccount = async (req: Request, res: Response) => {
+  // express validatior errors
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: errors.array() });
+  }
+
   const { email, password } = req.body;
 
+  // unique user
   const userExists = await User.findOne({ email });
-
   if (userExists) {
-    const error = new Error("User already exists");
+    const error = new Error("User email already exists");
+    return res.status(409).json({ error: error.message });
+  }
+
+  // unique handle
+  const handle = slug(req.body.handle, "");
+  const handleExists = await User.findOne({ handle });
+  if (handleExists) {
+    const error = new Error("Username already exists");
     return res.status(409).json({ error: error.message });
   }
 
   const user = new User(req.body);
   user.password = await hashPassword(password);
+  user.handle = handle;
 
   await user.save();
 
--- devTree/src/router.ts
+++ devTree/src/router.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAccount } from "./controllers/authController";
 
 const router = Router();
 
 // Auth & Register
-router.post("/auth/register", createAccount);
+router.post(
+  "/auth/register",
+  body("handle").notEmpty().withMessage("Handle cannot be empty"),
+  body("name").notEmpty().withMessage("Name cannot be empty"),
+  body("email").isEmail().withMessage("Email is not valid"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters length"),
+  createAccount,
+);
 
 export default router;
