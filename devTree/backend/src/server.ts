7b3f2d188fd38e598a2e9e7a7403c837bea12038
Diego Rocha <drochaesc@gmail.com>
2026-02-26 17:19:34 -0600

    backend: Cors Configuration

--- devTree/backend/src/server.ts
+++ devTree/backend/src/server.ts
@@ -1,11 +1,16 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import router from "./router";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
+
+connectDB();
 
 const app = express();
 
-connectDB();
+// CORS
+app.use(cors(corsConfig));
 
 app.use(express.json());
 
