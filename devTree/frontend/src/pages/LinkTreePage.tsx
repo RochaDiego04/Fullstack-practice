7f3119a04f17f970508f5136d4da7c3672081b34
Diego Rocha <drochaesc@gmail.com>
2026-03-16 15:54:56 -0600

    frontend: input validation and linked to react state

--- devTree/frontend/src/pages/LinkTreePage.tsx
+++ devTree/frontend/src/pages/LinkTreePage.tsx
@@ -1,15 +1,42 @@
 import { useState } from "react";
 import { social } from "../data/social";
 import DevTreeInput from "../components/DevTreeInput";
+import { isValidUrl } from "../utils";
+import { toast } from "sonner";
 
 export default function LinkTreePage() {
   const [devTreeLinks, setDevTreeLinks] = useState(social);
-  console.log(devTreeLinks);
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link,
+    );
+    setDevTreeLinks(updatedLinks);
+  };
+
+  const handleToggleChange = (itemName: string) => {
+    const updatedLinks = devTreeLinks.map((link) => {
+      if (link.name === itemName) {
+        if (isValidUrl(link.url)) {
+          return { ...link, enabled: !link.enabled };
+        } else {
+          toast.error("URL is not valid");
+        }
+      }
+      return link;
+    });
+    setDevTreeLinks(updatedLinks);
+  };
 
   return (
     <div className="space-y-5">
       {devTreeLinks.map((item) => (
-        <DevTreeInput key={item.name} item={item} />
+        <DevTreeInput
+          key={item.name}
+          item={item}
+          handleUrlChange={handleUrlChange}
+          handleToggleChange={handleToggleChange}
+        />
       ))}
     </div>
   );
