57568f1d0c45b5ec906acf1b0fe25af12ca60863
Diego Rocha <drochaesc@gmail.com>
2026-04-12 16:39:50 -0600

    chore: budget Routes REST API

---
+++ cashTrackr/backend/src/controllers/BudgetController.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from "express";
+
+export class BudgetController {
+  static getAll = async (req: Request, res: Response) => {
+    console.log("");
+  };
+  static create = async (req: Request, res: Response) => {
+    console.log("");
+  };
+  static getById = async (req: Request, res: Response) => {
+    console.log("");
+  };
+  static updateById = async (req: Request, res: Response) => {
+    console.log("");
+  };
+  static deleteById = async (req: Request, res: Response) => {
+    console.log("");
+  };
+}
--- cashTrackr/backend/src/server.ts
+++ cashTrackr/backend/src/server.ts
@@ -1,11 +1,26 @@
 import express from "express";
 import colors from "colors";
 import morgan from "morgan";
+import { db } from "./config/db";
+import budgetRouter from "./routes/budgetRouter";
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue.bold("Successful connection to DB"));
+  } catch (_error) {
+    console.log(colors.red.bold("Failed connection to DB"));
+  }
+}
+connectDB();
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use("/api/v1/budgets", budgetRouter);
 
 export default app;
