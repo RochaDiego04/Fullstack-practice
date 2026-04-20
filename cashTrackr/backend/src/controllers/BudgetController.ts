41448f3121a99a4b8323d3cab1da9b699d129b84
Diego Rocha <drochaesc@gmail.com>
2026-04-19 19:00:30 -0600

    feat: backend, implemented budget get getAll and create

--- cashTrackr/backend/src/controllers/BudgetController.ts
+++ cashTrackr/backend/src/controllers/BudgetController.ts
@@ -1,18 +1,49 @@
 import type { Request, Response } from "express";
+import Budget from "../models/Budget";
 
 export class BudgetController {
   static getAll = async (req: Request, res: Response) => {
-    console.log("");
+    try {
+      const budgets = await Budget.findAll({
+        order: [["createdAt", "DESC"]],
+        // TODO: Filter by authenticatd user
+      });
+
+      res.json(budgets);
+    } catch (error) {
+      res.status(500).json({ error: "There was an error" });
+    }
   };
+
   static create = async (req: Request, res: Response) => {
-    console.log("");
+    try {
+      const budget = new Budget(req.body);
+      await budget.save();
+      res.status(200).json("Budget created successfully");
+    } catch (error) {
+      res.status(500).json({ error: "There was an error" });
+    }
   };
+
   static getById = async (req: Request, res: Response) => {
-    console.log("");
+    try {
+      const { id } = req.params;
+      const budget = await Budget.findByPk(+id);
+
+      if (!budget) {
+        const error = new Error("Budget not found");
+        return res.status(404).json({ error: error.message });
+      }
+      res.json(budget);
+    } catch (error) {
+      res.status(500).json({ error: "There was an error" });
+    }
   };
+
   static updateById = async (req: Request, res: Response) => {
     console.log("");
   };
+
   static deleteById = async (req: Request, res: Response) => {
     console.log("");
   };
--- cashTrackr/backend/src/routes/budgetRouter.ts
+++ cashTrackr/backend/src/routes/budgetRouter.ts
@@ -1,11 +1,36 @@
 import { Router } from "express";
+import { body, param } from "express-validator";
 import { BudgetController } from "../controllers/BudgetController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
 router.get("/", BudgetController.getAll);
-router.post("/", BudgetController.create);
-router.get("/:id", BudgetController.getById);
+
+router.post(
+  "/",
+  body("name").notEmpty().withMessage("The budget's name cannot be empty"),
+  body("amount")
+    .notEmpty()
+    .withMessage("The amount quantity name cannot be empty")
+    .isNumeric()
+    .withMessage("Not valid quantity")
+    .custom((value) => value > 0)
+    .withMessage("Amount must be greater than 0"),
+  handleInputErrors,
+  BudgetController.create,
+);
+
+router.get(
+  "/:id",
+  param("id")
+    .isInt()
+    .withMessage("Invalid ID")
+    .custom((value) => value > 0)
+    .withMessage("Invalid negative ID"),
+  handleInputErrors,
+  BudgetController.getById,
+);
 router.patch("/:id", BudgetController.updateById);
 router.delete("/:id", BudgetController.deleteById);
 
