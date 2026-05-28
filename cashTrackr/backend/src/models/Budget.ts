7bae07ec7fda679e90831040a9fa0885d630b2af
Diego Rocha <drochaesc@gmail.com>
2026-05-27 22:33:55 -0600

    feat: backend, created Expense model

--- cashTrackr/backend/src/models/Budget.ts
+++ cashTrackr/backend/src/models/Budget.ts
@@ -1,12 +1,5 @@
-import {
-  Table,
-  Column,
-  DataType,
-  HasMany,
-  BelongsTo,
-  ForeignKey,
-  Model,
-} from "sequelize-typescript";
+import { Table, Column, DataType, HasMany, Model } from "sequelize-typescript";
+import Expense from "./Expense";
 
 @Table({
   tableName: "budgets",
@@ -21,6 +14,17 @@ class Budget extends Model {
     type: DataType.DECIMAL,
   })
   declare amount: number;
+
+  @HasMany(() => Expense, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  declare expenses: Expense[];
+  // If removed Budget, remove all the expenses related to it.
+  // RESTRICT if you need to remove all expenses related to the budget first.
 }
 
 export default Budget;
+
+// A budget can have multiple expenses
+// Budget.expenses [array with multiple expenses]
