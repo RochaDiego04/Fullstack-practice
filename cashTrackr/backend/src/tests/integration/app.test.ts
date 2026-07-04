8581cbd7036332a526fcfdf399ca2c0a44fc6666
Diego Rocha <drochaesc@gmail.com>
2026-07-04 00:53:43 -0600

    chore: frontend init files

--- cashTrackr/backend/src/tests/integration/app.test.ts
+++ cashTrackr/backend/src/tests/integration/app.test.ts
@@ -23,4 +23,18 @@ describe("Authentication - Create Account", () => {
     expect(response.body.errors).toHaveLength(3);
     expect(createAccountMock).not.toHaveBeenCalled();
   });
+
+  it("should return invalid email", async () => {
+    // learning purpose, usually we dont want to test external library behavior
+    const response = await request(server)
+      .post("/api/v1/auth/create-account")
+      .send({ name: "Diego", password: "12345678", email: "not_valid_email" });
+    const createAccountMock = jest.spyOn(AuthController, "createAccount");
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(1);
+    expect(createAccountMock).not.toHaveBeenCalled();
+  });
+
 });
