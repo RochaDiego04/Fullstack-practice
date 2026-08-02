20b4dd9785aa14690602a8ff21ceb8855c379bf8
Diego Rocha <drochaesc@gmail.com>
2026-08-01 19:35:42 -0600

    feat frontend: added dynamic expenseforms and buttons before applying server actions

---
+++ cashTrackr/frontend/app/admin/budgets/[id]/page.tsx
@@ -0,0 +1,41 @@
+import { getBudgetById } from "@/src/services/budgets";
+import { Metadata } from "next";
+import AddExpenseButton from "../../../../components/expenses/AddExpenseButton";
+import ModalContainer from "../../../../components/ui/ModalContainer";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const { id } = await params;
+  const budget = await getBudgetById(id);
+  return {
+    title: `CashTrackr - ${budget.name}`,
+  };
+}
+
+export default async function BudgetDetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = await params;
+  const budget = await getBudgetById(id);
+
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="font-black text-4xl text-purple-950">{budget.name}</h1>
+          <p className="text-xl font-bold">
+            Administra tus <span className="text-amber-500">gastos</span>
+          </p>
+        </div>
+        <AddExpenseButton />
+      </div>
+
+      <ModalContainer />
+    </>
+  );
+}
